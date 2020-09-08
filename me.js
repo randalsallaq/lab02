906a946ef526f4e70c7bcd9db8e67fb20fc86307
rand alsallaq <randalsallaq97@gmail.com>
2020-09-08 17:35:15 +0300

    I added two q and the result

--- me.js
+++ me.js
@@ -1,7 +1,9 @@
 'use strict';
 
-// this is the greeting, in addition to the user's name in order to make more interaction with the user.//
-alert( 'hello there!');
+var result = 0;
+
+// // this is the greeting, in addition to the user's name in order to make more interaction with the user.//
+// alert( 'hello there!');
 
 var username = prompt('What is your name?');
 console.log('welcome' + username);
@@ -11,39 +13,47 @@ alert('welcome ' + username);
 
 
 
-//Q1
-//here i am gonna start the guessing game by asking the user if they know me.//
-//if the answer is yes then it will show them: "hello friend"//
-//otherwise it will show them "I will be glad to let you know me"//
+// //Q1
+// //here i am gonna start the guessing game by asking the user if they know me.//
+// //if the answer is yes then it will show them: "hello friend"//
+// //otherwise it will show them "I will be glad to let you know me"//
+
+
 var meetme = prompt('Do You Know Me?'); 
 if (meetme.toLowerCase() === 'y' || meetme.toLowerCase() === 'yes') {
     //console.log('Hi Friend');
     alert('Hi Friend');
+    result == result++;
   } else {
       //console.log('I will be glad to let you know me');
     alert('I will be glad to let you know me');
   }
 
 
-//Q2
-//in this question u am asking the user to guess id i am more than 23 years old//
-// if his answer is yes then it'll show this: 'WOW. You\'re smart'//
-//otherwise it'll show: 'wrong. I am 23 and 6 months-_-'//
+// //Q2
+// //in this question u am asking the user to guess id i am more than 23 years old//
+// // if his answer is yes then it'll show this: 'WOW. You\'re smart'//
+// //otherwise it'll show: 'wrong. I am 23 and 6 months-_-'//
+
+
 
   var age = prompt('Am I more than 23?');
   if (age.toLowerCase() === 'y' || age.toLowerCase() === 'yes') {
       //console.log('WOW. You\'re smart');
       alert('WOW. You\'re smart');
+      result == result++;
   } else {
       //console.log('wrong. I am 23 and 6 months-_-');
     alert('wrong. I am 23 and 6 months-_-')
   }
 
 
-  //Q3
-  //in this question i am asking the user if i am an employee//
-  //if the answer is yes then it'll show: 'NOPE :D'//
-  //otherwise it'll show:'YOU GOT IT'//
+//   //Q3
+//   //in this question i am asking the user if i am an employee//
+//   //if the answer is yes then it'll show: 'NOPE :D'//
+//   //otherwise it'll show:'YOU GOT IT'//
+
+
 
     var employee = prompt('Do you think I am an employee?');
     if (employee.toLowerCase() === 'y' || employee.toLowerCase() === 'yes') {
@@ -52,7 +62,9 @@ if (meetme.toLowerCase() === 'y' || meetme.toLowerCase() === 'yes') {
     } else {
        // console.log('YOU GOT IT');
         alert('YOU GOT IT');
+        result == result++;
     }
+
 
     //Q4
     //here i am asking the user if I love potato//
@@ -63,12 +75,13 @@ if (meetme.toLowerCase() === 'y' || meetme.toLowerCase() === 'yes') {
     if (potato.toLowerCase() === 'y' || potato.toLowerCase() === 'yes') {
        // console.log('absolutely!');
         alert('absolutely!');
+        result == result++;
     } else {
        // console.log('is it possible not to love potato even!');
         alert('is it possible not to love potato even!');
     }
 
-    //Q5
+//     //Q5
     //here i am asking the user if they do like the coconut perfumes//
     //if they do then it'll show them: 'Finally! Someone shares me this!'//
     //otherwise it'll show: 'as usual'//
@@ -77,8 +90,56 @@ if (meetme.toLowerCase() === 'y' || meetme.toLowerCase() === 'yes') {
     if (perfume.toLowerCase() === 'y' || perfume.toLowerCase() === 'yes') {
         //console.log('Finally! Someone shares me this!');
         alert('Finally! Someone shares me this!');
+        result == result++;
     } else {
        // console.log('as usual');
         alert('as usual');
        
     }
+
+    //Q6
+   
+    
+    for (var i = 0; i < 4; i++) {
+      var brothers = prompt('how many brothers do I have? , you have 6 attempts');
+      if (brothers < 4) {
+        brothers = Number(brothers);
+        //console.log('too low');
+        alert('too low');
+      }
+        else if ( brothers > 4) {
+          //console.log('too high');
+          alert('too high')
+        }
+       else{
+       // console.log('correct');
+        alert('correct');
+        result == result++;
+        break;
+        
+      }
+    }
+
+//Q7
+var animals = ['cat', 'dog', 'flamingo'];
+    for (var o = 0; o < 6; o++){
+      var guessfav = prompt ('what is my favourite animal?');
+      
+      if (guessfav == 'cat' || guessfav == 'dog' || guessfav == 'flamingo') {
+        alert('correct!');
+        result == result++;
+      break;
+      
+      } else {
+        alert('lets try again');
+      }
+      
+      
+    } 
+    
+    alert('my fav animals are : cat , dog , flamingo ');
+
+    
+      alert('your result is ' + result);
+    
+
